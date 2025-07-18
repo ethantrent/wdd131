f17c2f2da1cae782f598e09aa5332116f89ce71f
Ethan <ethanjotrent@gmail.com>
2025-07-17 20:43:10 -0600

    event-listeners

---
+++ master-js/event-listeners/event-listener.js
@@ -0,0 +1,7 @@
+// .addEventListener attaches event handlers to elements
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", () => {
+    console.log("I want to open the box!")
+})
